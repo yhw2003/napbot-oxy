d5fd474e076784cfc04ca7cd2e56036dae8bc739
yhw2003 <321612813@qq.com>
2026-01-21 22:33:10 +0800

    update

--- src/api/type-wrapper/base.ts
+++ src/api/type-wrapper/base.ts
@@ -1,10 +1,27 @@
 import { z } from "zod";
 
+/**
+ * 流式返回标记
+ */
+export const StreamFlagSchema = z.enum([
+    "normal-action",
+    "stream-action",
+]);
+export type Stream = z.infer<typeof StreamFlagSchema>;
+
+/**
+ * Api返回值的基础类
+ */
 export const ApiBaseSchema = z.object({
-    status: z.string(),
-    retcode: z.int(),
-    data: z.any()
-})
+    "data": z.record(z.string(), z.any()),
+    "echo": z.union([z.null(), z.string()]).optional(),
+    "message": z.string(),
+    "retcode": z.number(),
+    "status": z.string(),
+    /** 流式返回标记 */
+    "stream": StreamFlagSchema,
+    "wording": z.union([z.null(), z.string()]).optional(),
+});z.any()
+
 
 export type ApiBase = z.infer<typeof ApiBaseSchema>;
-
